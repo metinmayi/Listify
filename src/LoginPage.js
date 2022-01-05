0b36e9eeb8bd1b5ae9803b41ea6d88c27ebf67e7
Metin <metinmayi@gmail.com>
2022-01-05 17:01:02 +0100

    Restructured code

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,19 +1,46 @@
 import Footer from "./Footer";
-import LoginBox from "./LoginBox";
 import DefaultButton from "./DefaultButton";
-import { Link } from "react-router-dom";
+import Input from "./Input";
+import styled from "styled-components";
+// import { Link } from "react-router-dom";
+
+//#region Functions
+const Login = () => {};
+//#endregion
+
 const LoginPage = () => {
 	return (
-		<div className="LoginPage">
-			<div className="LoginPageFooter">
-			<div className="LogoContainer"></div>
-			<h2>Sign in to Listify</h2>
+		<div className="Page">
+			<div className="LoginPageHeader">
+				<LogoContainer></LogoContainer>
+				<h2>Sign in to Listify</h2>
 			</div>
-			<LoginBox />
-			<Link to="/mainpage"><DefaultButton text="Login" /></Link>
+			<LoginBox>
+				<Input label="E-mail" type="text" id="emailInput" />
+				<Input label="Password" type="password" id="passwordInput" />
+			</LoginBox>
+			<DefaultButton onClick={Login}>Login</DefaultButton>
 			<Footer />
 		</div>
 	);
 };
 
 export default LoginPage;
+
+//#region Declarations
+const LogoContainer = styled.div`
+	background-image: url("https://i.ibb.co/0yPv7mr/logo.png");
+	background-size: contain;
+	background-position: center;
+	background-repeat: no-repeat;
+	height: 30vh;
+`;
+
+const LoginBox = styled.div`
+	display: flex;
+	justify-content: stretch;
+	flex-direction: column;
+	width: 50%;
+	font-size: 1.5rem;
+`;
+//#endregion
