aa3f8717253aee4e8fc107ad5a5b2cd5e07b2472
Metin <metinmayi@gmail.com>
2022-01-06 21:34:20 +0100

    Changed footer to a styled component. Removed styling from CSS file

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,21 @@
+import styled from "styled-components";
 const Footer = () => {
 	return (
-		<div className="Footer">
-			<p>Looking for help?</p>
-			<p>Contact me at</p>
-			<p>metinmayi@gmail.com</p>
-		</div>
+		<FooterComponent>
+			<FooterP>Looking for help?</FooterP>
+			<FooterP>Contact me at</FooterP>
+			<FooterP>metinmayi@gmail.com</FooterP>
+		</FooterComponent>
 	);
 };
 
 export default Footer;
+
+const FooterComponent = styled.div`
+	font-size: 1rem;
+	text-align: center;
+`;
+const FooterP = styled.p`
+	margin-block-start: 0;
+	margin-block-end: 0;
+`;
