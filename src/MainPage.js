d6a9642e055c48e533ec6144633ec60b917133ef
Metin <metinmayi@gmail.com>
2022-01-17 11:41:13 +0100

    MainPage now navigates away if loggedIn == false

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,10 +1,18 @@
 import Header from "./Header.js";
 import Main from "./Main.js";
-const MainPage = ({ logoutFunction }) => {
+import Footer from "./Footer.js";
+import { useContext } from "react";
+import LoginContext from "./context/LoginContext.js";
+import { Navigate } from "react-router-dom";
+const MainPage = () => {
+	const { logoutFunction } = useContext(LoginContext);
+	const { loggedIn } = useContext(LoginContext);
 	return (
 		<>
+			{!loggedIn && <Navigate to="/" />}
 			<Header logoutFunction={logoutFunction} />
 			<Main />
+			<Footer></Footer>
 		</>
 	);
 };
