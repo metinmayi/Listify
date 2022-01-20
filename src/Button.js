7b7f4054d898215ee9fc7eeb3a99a640a6d34de7
Metin <metinmayi@gmail.com>
2022-01-20 21:08:11 +0100

    Remade buttons. From 4 files to 1 variety button

---
+++ src/Button.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+const Button = ({ children, variant, onClick }) => {
+	return (
+		<Primary variant={variant} onClick={onClick ? (e) => onClick(e) : null}>
+			{children}
+		</Primary>
+	);
+};
+
+export default Button;
+const Primary = styled.button`
+	background-color: #64b9ee;
+	border-radius: 10px;
+	margin-top: 5px;
+	border: none;
+	height: 5vh;
+	font-size: 1rem;
+	flex-grow: 1;
+	font-weight: 400;
+	font-family: "Roboto Slab";
+	${({ variant }) =>
+		variant === "secondary"
+			? css`
+					background-color: #e2e54f;
+			  `
+			: null}
+`;
